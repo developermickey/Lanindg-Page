de0084413c24bba77ebee55e0b3bd715c4daab04
mickeysrishti <Mukeshpathak345@gmail.com>
2023-10-03 18:17:15 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+function isEligible(age) {
+return age >=18;
+
+    // if (age >=18) {
+    //     return true;
+    // }
+    // else {
+    //     return false;
+    // }
+}
+
+console.log(isEligible(10))
